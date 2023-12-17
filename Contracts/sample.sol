aadb56654e1360e91109b768cceb2e5927c12d76
cybertrucker-erc <153589741+cybertrucker-erc@users.noreply.github.com>
2023-12-17 13:30:17 -0500

    Add files via upload

---
+++ Contracts/sample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Sample {
+    uint public required;
+
+    constructor(uint _required){
+        required = _required;
+    }
+}
